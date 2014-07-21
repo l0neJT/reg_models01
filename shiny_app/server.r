03be8388dc190de34d1c3f431c909580a5a9ff9e
l0neJT <loganjtravis@gmail.com>
2014-07-20 19:05:54 -0700

    Create Shiny App Stub Files
    Create shiny application stub files including README.md

---
+++ shiny_app/server.r
@@ -0,0 +1,11 @@
+## Shiny Application for Exploring MTCARS Data Through Linear Regression
+## John's Hopkins University Developing Data Products on Coursera
+## Logan J Travis
+## 2014-07-27
+
+# Load libraries
+library(shiny)
+
+shinyServer(function(input, output) {
+    
+})
---
+++ shiny_app/ui.r
@@ -0,0 +1,22 @@
+## Shiny Application for Exploring MTCARS Data Through Linear Regression
+## John's Hopkins University Developing Data Products on Coursera
+## Logan J Travis
+## 2014-07-27
+
+# Load libraries
+library(shiny)
+
+# Define UI
+shinyUI(pageWithSidebar(
+    
+    # Add header with tile
+    headerPanel("Explore MTCARS Data Through Linear Regression")
+    
+    # Add sidebar for input
+    sidebarPanel(
+        )
+    
+    # Add main panel for plot
+    mainPanel(
+        )
+    ))
